b1904489558eaa939e86f42f627aa1da73f918ab
NelsinT <nelsoncunha2101@gmail.com>
2025-02-17 14:34:46 +0000

    teste

--- TrabalhosPraticos/ex_6_ficha1.c
+++ TrabalhosPraticos/ex_6_ficha1.c
@@ -26,24 +26,56 @@ void print_matriz(int m3[linhas][colunas])
 void calcular_media(int m3[linhas][colunas])
 {
     int soma = 0;
+    int soma_coluna = 0;
     float media;
+    float media_coluna;
     for (int i = 0; i < linhas; i++)
     {
         for (int j = 0; j < colunas; j++)
         {
+
             soma += m3[i][j];
         }
 
         media = soma / (linhas * colunas);
     }
     printf("a soma e  %d\n ", soma);
-    printf("A media e : %f", media);
-
+    printf("A media e : %f \n", media);
 }
+void calcular_media_coluna(int m3[linhas][colunas])
+{
+    int soma = 0;
+    float media_coluna = 0;
+    int c = 1;
+    for (int i = 0; i < linhas; i++)
+
+    {
+        soma += m3[i][c];
+    }
+    media_coluna = soma / (linhas);
+    printf("A media da coluna e  : %f",media_coluna);
+}
+
+void calcular_media_linha(int m3[linhas][colunas])
+{
+    int soma = 0;
+    float media_linha = 0;
+    int c = 1;
+    for (int j = 0; j < colunas; j++)
+
+    {
+        soma += m3[c][j];
+    }
+    media_linha= soma / (colunas);
+    printf("A media da linha e  : %f",media_linha);
+}
+
 int main()
 {
     int mat1[linhas][colunas];
     ler_matriz(mat1);
     print_matriz(mat1);
     calcular_media(mat1);
+    calcular_media_coluna(mat1);
+    calcular_media_linha(mat1);
 }
